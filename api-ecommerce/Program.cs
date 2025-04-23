1b44db6fe14d8a9c9239b78bca9d0d14cbb9ade9
joashneves <joashneves@gmail.com>
2025-04-23 15:25:48 -0300

    Colocado versionamento

--- api-ecommerce/Program.cs
+++ api-ecommerce/Program.cs
@@ -1,3 +1,7 @@
+using api_ecommerce.SwaggerConfig;
+using Asp.Versioning.ApiExplorer;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +9,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(option =>
+{
+    option.OperationFilter<SweggerDefaultValue>();
+});
+builder.Services.AddApiVersioning().AddMvc().AddApiExplorer(setup =>
+{
+    setup.GroupNameFormat = "'v'VVV";
+    setup.SubstituteApiVersionInUrl = true;
+});
+builder.Services.ConfigureOptions<ConfigureSwaggerGenOptions>();
 
 var app = builder.Build();
 
@@ -13,7 +26,14 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(option =>
+    {
+        var version = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+        foreach(var description in version.ApiVersionDescriptions)
+        {
+            option.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", $"e-commerce {description.GroupName.ToUpper()}");
+        }
+    });
 }
 
 app.UseHttpsRedirection();
---
+++ api-ecommerce/SwaggerConfig/ConfigureSwaggerGenOptions.cs
@@ -0,0 +1,38 @@
+﻿using Asp.Versioning.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace api_ecommerce.SwaggerConfig
+{
+    public class ConfigureSwaggerGenOptions : IConfigureOptions<SwaggerGenOptions>
+    {
+        private readonly IApiVersionDescriptionProvider _apiVersionDescriptionProvider;
+
+        public ConfigureSwaggerGenOptions(IApiVersionDescriptionProvider apiVersionDescriptionProvider)
+        {
+            _apiVersionDescriptionProvider = apiVersionDescriptionProvider ?? throw new ArgumentNullException(nameof(apiVersionDescriptionProvider));
+        }
+
+        public void Configure(SwaggerGenOptions options)
+        {
+            foreach (var description in _apiVersionDescriptionProvider.ApiVersionDescriptions)
+            {
+                options.SwaggerDoc(description.GroupName, CreateOpenApiInfo(description));
+            }
+        }
+        private static OpenApiInfo CreateOpenApiInfo(ApiVersionDescription description)
+        {
+            var info = new OpenApiInfo()
+            {
+                Title = "API E-commerce",
+                Version = description.ApiVersion.ToString(),
+            };
+            if (description.IsDeprecated)
+            {
+                info.Description += " This API version has been deprecated.";
+            }
+            return info;
+        }
+    }
+}
