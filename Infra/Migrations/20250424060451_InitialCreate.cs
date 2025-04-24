6c1a93fbfeb1fc00485b1d95db1bf738638ad912
joashneves <joashneves@gmail.com>
2025-04-24 03:13:13 -0300

    ajeita migration

---
+++ Infra/Migrations/20250424060451_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProdutoSet",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "varchar", nullable: false),
+                    Nome = table.Column<string>(type: "text", nullable: false),
+                    Descricao = table.Column<string>(type: "text", nullable: false),
+                    Preco = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false),
+                    Categoria = table.Column<string>(type: "text", nullable: true),
+                    Quantidade = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProdutoSet", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProdutoSet");
+        }
+    }
+}
--- Infra/ProdutoContext.cs
+++ Infra/ProdutoContext.cs
@@ -7,31 +7,49 @@ namespace Infra
 {
     public class ProdutoContext : DbContext
     {
-        private IConfiguration _configuration;
+        public ProdutoContext() { }
 
         // DbSet para acessar os produtos no banco de dados
-        public DbSet<Produto> ProdutoSet { get; set; } // Alterado de ContaSet para ProdutoSet
 
-        public ProdutoContext(IConfiguration configuration, DbContextOptions<ProdutoContext> options)
+        public ProdutoContext(DbContextOptions<ProdutoContext> options)
             : base(options)
         {
-            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
         }
+        public DbSet<Produto> ProdutoSet { get; set; } // Alterado de ContaSet para ProdutoSet
 
         // Configuração da string de conexão e do banco de dados
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             // Comentado o código de teste para exibir a string de conexão no console.
             // Utilizando SQL Server, substitua a string de conexão conforme necessário.
-            string? connectionString = Environment.GetEnvironmentVariable("SQLData"); // Variável de ambiente
+            string? connectionString = Environment.GetEnvironmentVariable("SQLData");
             if (string.IsNullOrEmpty(connectionString))
             {
-                // Se não encontrar a variável de ambiente, use uma string de conexão padrão
-                connectionString = _configuration.GetConnectionString("SQLData"); // A configuração do appsettings.json
-                Console.WriteLine("connectionString: " + connectionString);
+                // Fallback (evita erro se a variável não estiver setada)
+                connectionString = "Server=localhost;Port=5432;Database=local_db;User Id=local_user;Password=local_password;"; // Ajuste conforme seu ambiente
             }
+
             // Usando SQL Server (substitua a string de conexão conforme necessário)
-            optionsBuilder.UseSqlServer(connectionString);
+            optionsBuilder.UseNpgsql(connectionString);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+
+        {
+
+            modelBuilder.Entity<Produto>(entity =>
+            {
+                entity.Property(p => p.Id)
+                .HasColumnName("id")
+                .HasColumnType("varchar") // ou "text", dependendo do seu banco
+                .IsRequired();
+
+
+                entity.Property(p => p.Nome)
+
+                .HasColumnName("Nome");
+
+            });
+
         }
     }
-}
+    }
